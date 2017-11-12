452eb696dbf626177f0f8e8d16cf9136952b28bd
Andrew Mollica <a.r.mollica@gmail.com>
2017-11-12 13:34:17 -0600

    Make rename command simpler and more general

--- tidytable/commands/rename_cmd.py
+++ tidytable/commands/rename_cmd.py
@@ -1,40 +1,23 @@
 import click
 import re
 import pandas as pd
-import inflection
 from tidytable.util import processor
 
-sanitizers = {
-    'snake-case': inflection.underscore,
-    'camel-case': inflection.camelize,
-    'humanize': inflection.humanize,
-    'titleize': inflection.titleize
-}
-
 @click.command('rename')
-@click.option('-f', '--find', type = click.STRING)
-@click.option('-r', '--replace', type = click.STRING)
-@click.option('-s', '--sanitize',
-              type = click.Choice(['snake-case', 'camel-case', 'humanize', 'titleize']))
-@click.option('-m', '--mapping', type = click.STRING)
+@click.option('-m', '--map', 'map_expression', type = click.STRING)
+@click.option('-a', '--assign', type = click.STRING)
 @processor
-def cli(dfs, find, replace, sanitize, mapping):
+def cli(dfs, assign, map_expression):
     '''
-    Rename columns. Provide a comma-separated list of column names mappings, e.g., NEW = OLD.
+    Rename columns. Provide a comma-separated list of column names mappings, e.g., NEW <- OLD.
     '''
     for df in dfs:
-        if (find is not None) and (replace is not None):
-            df.columns = map(lambda column: re.sub(find, replace, column), df.columns)
-
-        if sanitize is not None:
-            sanitizer = sanitizers[sanitize]
-            df.columns = map(lambda column: sanitizer(column), df.columns)
-        
-        if mapping is not None:
+        if map_expression is not None:
+            df.columns = map(eval('lambda d: ' + map_expression), df.columns)
+        if assign is not None:
             columns = dict()
-            for chunk in mapping.split(','):
-                names = chunk.split('=')
+            for chunk in assign.split(','):
+                names = chunk.split('<-')
                 columns[names[1].strip()] = names[0].strip()
             df = df.rename(columns = columns)
-        
         yield df
