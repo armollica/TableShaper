03ffcdf7ba56cc6bc124585ca629dbfb34e5a232
Andrew Mollica <a.r.mollica@gmail.com>
2018-05-26 22:36:49 -0500

    Add multiple file support with input command

--- tableshaper/cli_commands/input.py
+++ tableshaper/cli_commands/input.py
@@ -1,5 +1,4 @@
-import os
-import click
+import os, sys, glob, click
 import pandas as pd
 
 def filename_to_tablename(filename):
@@ -12,7 +11,7 @@ def filename_to_tablename(filename):
               type=click.Choice(['csv', 'tsv', 'json']))
 @click.option('-r', '--raw', 'raw', flag_value='raw',
               help = "Don't guess data types")
-@click.argument('file', type=click.File('rb'))
+@click.argument('file', type=click.STRING)
 @click.pass_context
 def cli(context, name, type, raw, file):
     '''
@@ -23,24 +22,37 @@ def cli(context, name, type, raw, file):
     dtype = None
     if raw:
         dtype = str
-    
+
     # Read the table
     def read_table(file):
-        if type == 'json':
-            return pd.read_json(file, orient='records')
-        elif type == 'tsv':
-            return pd.read_csv(file, sep='\t', dtype=dtype)    
-        elif type == 'csv':
-            return pd.read_csv(file, dtype=dtype)
-    try:
-        table = read_table(file.name)
-    except Exception as e:
-        click.echo('Could not read "{}": {}'.format(file.name, e), err=True)
+        try:
+            if type == 'json':
+                return pd.read_json(file, orient='records')
+            elif type == 'tsv':
+                return pd.read_csv(file, sep='\t', dtype=dtype)    
+            elif type == 'csv':
+                return pd.read_csv(file, dtype=dtype)
+        except Exception as e:
+            click.echo('Could not read "{}": {}'.format(file, e), err=True)
+
+    if file == '-':
+        table = read_table(sys.stdin)
+    else:
+        filenames = glob.glob(file)
+        n = len(filenames)
+        for i, filename in enumerate(filenames):
+            table = read_table(filename)
+            
+            # Determine what to name the table
+            if name:
+                if n == 1:
+                    tablename = name
+                else:
+                    tablename = '{name}-{i}'.format(name=name, i=i)
+            else:
+                tablename = filename_to_tablename(filename)
+            
+            # Add the table to the list and make it the current target
+            context.obj['add_table'](tablename, table)
+            context.obj['set_target'](tablename)
     
-    if table is not None:
-        # Add the table and make it the current target
-        tablename = filename_to_tablename(file.name)
-        if name:
-            tablename = name
-        context.obj['add_table'](tablename, table)
-        context.obj['set_target'](tablename)
