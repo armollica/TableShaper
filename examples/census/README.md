80134d82ad5521cfdc740c62b9d6481172925aa2
Andrew Mollica <a.r.mollica@gmail.com>
2019-12-13 15:18:13 -0600

    Fix typo in readme. Reorganize the census example.

---
+++ examples/census/README.md
@@ -0,0 +1,7 @@
+# Census example
+
+Download, clean up and map data from the U.S. Census Bureau using TablesShaper and [mapshaper](https://mapshaper.org/).
+
+The map shows the percent of county population that's foreign born.
+
+To run the example make sure you've got TableShaper and mapshaper both installed. Then run `bash create-map.sh`.
--- examples/census/create-map.sh
+++ examples/census/create-map.sh
@@ -1,9 +1,42 @@
-#!/usr/bin/env bash
+#!/usr/bin/env
 
-if [ ! -e data/counties-with-foreign-born.json ]; then
-    bash get-data.sh
+mkdir -p data
+
+cd data
+
+# Info on the Census API:
+# https://www.census.gov/data/developers/data-sets/acs-5year.html
+if [ ! -e raw-data.json ]; then
+    curl -o raw-data.json 'https://api.census.gov/data/2016/acs/acs5?get=NAME,B05002_001E,B05002_013E&for=county:*'
 fi
 
+if [ ! -e counties.tar.gz ]; then
+    curl -o counties.tar.gz 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Small-scale/data/Boundaries/countyp010g.shp_nt00934.tar.gz'
+fi
+
+if [ ! -e countyp010g.shp ]; then
+    tar -xzm -f counties.tar.gz
+fi
+
+# Clean up the data and join to the county geography
+tableshaper \
+    input -f json -j values -n foreign_born \
+        -c 'name, population, foreign_born, state, county' raw-data.json \
+    filter -s 2: \
+    mutate -r 'fips = format_text("{state}{county}")' \
+    mutate 'pct_foreign_born = parse_float(foreign_born) / parse_float(population)' \
+    pick 'fips, pct_foreign_born' \
+    input -f shp -n counties countyp010g.shp \
+    rename 'fips = ADMIN_FIPS' \
+    mutate 'geometry = geometry.simplify(0.01)' \
+    pick 'fips, geometry' \
+    filter -r 'fips[2:] != "000"' \
+    join --left -k fips foreign_born \
+    output -f geojson counties-with-foreign-born.json
+
+cd ..
+
+# Draw the map
 mapshaper \
     -i data/counties-with-foreign-born.json \
     -colorizer name=getColor \
